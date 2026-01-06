f864bd61cce27cacc462f22cbf15242509e4eee2
Dasun Cooray <dasuncooray48@gmail.com>
2026-01-07 01:50:47 +0530

    initial commit

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,88 @@
+import { User } from "../models/User";
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: "All fields must fill!" });
+        }
+
+        const existUser = await User.findOne({ email: email.toLowerCase() });
+
+        if (existUser) {
+            return res.status(400).json({ message: "User already exists!" });
+        }
+
+        const user = await User.create({
+            name,
+            email: email.toLowerCase(),
+            password,
+            role: role || "student"
+
+        })
+
+        res.status(201).json({
+            message: "User registered successfully!",
+            user: { id: user._id, name: user.name, email: user.email }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error", error: error.message
+        });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields must fill!" });
+        }
+
+        const user = await User.findOne({ email: email.toLowerCase() });
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found!" });
+        }
+
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) return res.status(400).json({ message: "Invalid Credentials" });
+
+        res.status(200).json({
+            message: "User logged successfully!",
+            user: {
+                id: user._id,
+                email: user.email,
+                name: user.name,
+                role: user.role
+            }
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error!" });
+    }
+}
+
+const logoutUser = async (req, res) => {
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "User not found!" });
+        }
+
+        res.status(200).json({
+            message: "Logged out successfully!"
+        })
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error!" });
+    }
+}
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
---
+++ backend/src/models/User.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+    },
+    
+    role: {
+        type: String,
+        required: true,
+        enum: ["student", "instructor"],
+        default: "student"
+    },
+},
+    {
+        timestamps: true
+    }
+
+)
+
+userSchema.pre("save", async function () {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
